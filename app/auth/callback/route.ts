1a45dbd714ea205fd66059d0f671f3c57f671513
Alexander Harm <alexjason.harm@gmail.com>
2024-12-25 16:15:51 +0100

    Fixed

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -5,7 +5,6 @@ export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
   const next = searchParams.get('next') ?? '/'
-  console.log('code:', code)
 
   if (code) {
     const supabase = await createClient()
