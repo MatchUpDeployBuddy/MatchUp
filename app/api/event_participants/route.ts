5fd4c9808308eca6810abc43501c3a715bde0183
jannik <jascit05@hs-esslingen.de>
2025-01-08 22:27:40 +0100

    add route for retrieving all event participants

---
+++ app/api/event_participants/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import { createClient } from "@/utils/supabase/server";
+
+// Beispiel: GET /api/event_participants?eventId=60199094-8cf5-485d-9361-829f9eb8b339
+
+export async function GET(request: Request): Promise<NextResponse> {
+  try {
+    const supabase = await createClient();
+    const { searchParams } = new URL(request.url);
+
+    const eventId = searchParams.get("eventId");
+
+    if (!eventId) {
+      return NextResponse.json(
+        { error: "Missing eventId parameter" },
+        { status: 400 }
+      );
+    }
+
+    const { data, error } = await supabase.rpc("get_event_participants_names", {
+      event_uuid: eventId,
+    });
+
+    if (error) {
+      console.error("Error executing Supabase RPC:", { error, eventId });
+      return NextResponse.json(
+        { error: "Failed to fetch participants from the database" },
+        { status: 500 }
+      );
+    }
+
+    return NextResponse.json({ participants: data }, { status: 200 });
+  } catch (err) {
+    console.error("Server error:", err);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
