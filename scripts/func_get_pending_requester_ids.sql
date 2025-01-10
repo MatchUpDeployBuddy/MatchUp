28d129c1f574ff98ac4c581cc7a3fa4589d35f58
jannik <jascit05@hs-esslingen.de>
2025-01-10 13:36:53 +0100

    rename sql file

---
+++ scripts/func_get_pending_requester_ids.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE FUNCTION public.get_pending_requester_ids(event_uuid UUID)
+RETURNS TABLE(requester_id UUID, user_name TEXT) AS $$
+BEGIN
+  RETURN QUERY
+  SELECT er.requester_id, u.name::TEXT
+  FROM public.event_requests er
+  JOIN public.users u ON er.requester_id = u.id
+  WHERE er.event_id = event_uuid 
+  AND er.status = 'pending';
+END;
+$$ LANGUAGE plpgsql;
